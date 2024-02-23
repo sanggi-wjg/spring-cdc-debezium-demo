6875b1393c93b2766c51acf9b2f9f761ce5e9896
Raynor <jonggu.woo@fitpet.co.kr>
2024-02-23 15:42:47 +0900

    2024 02 23

--- docker/Tutorial.md
+++ docker/Tutorial.md
@@ -2,6 +2,21 @@
 
 ## Procedure
 
+### Master-Slave Replication
+
+```shell
+# [Master]
+show master status;
+
+# [Slave]
+STOP SLAVE;
+# container ip 변경
+CHANGE MASTER TO MASTER_HOST='cdc-demo-mysql-master', MASTER_USER='root', MASTER_PASSWORD='rootroot', MASTER_LOG_POS=157, MASTER_LOG_FILE='mysql-bin.000003';
+
+START SLAVE;
+SHOW SLAVE STATUS \G;
+```
+
 ### Register the Debezium MySQL connector
 
 ```sh
--- src/main/kotlin/com/example/springcdcdebeziumdemo/consumer/ProducerService.kt
+++ src/main/kotlin/com/example/springcdcdebeziumdemo/consumer/ProducerService.kt
@@ -1,6 +1,7 @@
 package com.example.springcdcdebeziumdemo.consumer
 
 import com.example.springcdcdebeziumdemo.kafka.KafkaTopic
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Service
 
@@ -13,8 +14,11 @@ class BasicProducerService(
     private val kafkaProducer: KafkaTemplate<String, Any>
 ) : ProducerService {
 
+    private val log = LoggerFactory.getLogger(this::class.java)
+
 //    @TransactionalEventListener
     override fun publishTestMessage() {
+        log.info("Publishing test message")
         kafkaProducer.send(KafkaTopic.TEST, "test")
     }
 }
